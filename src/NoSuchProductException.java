111879b49f98b8a3bc33a1ed8c5b641ae9cd572c
buzzbuzzyo <58431113+buzzbuzzyo@users.noreply.github.com>
2019-12-05 23:55:01 +0800

    nosuchproduct

--- src/NoSuchProductException.java
+++ src/NoSuchProductException.java
@@ -6,8 +6,8 @@ public class NoSuchProductException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public NoSuchProductException(String string) {
-		super ("No such product! Please use ShowShopProduct command to show all product in shop.");
+	public NoSuchProductException(String message) {
+		super (message);
 	}
 
 	public NoSuchProductException() {
